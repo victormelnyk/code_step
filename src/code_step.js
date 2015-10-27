2f30f14c3febe4ff48ab284505989a17d82042d2
Victor Melnyk <victor.melnyk.vm@gmail.com>
2015-10-27 23:08:18 +0200

    implement init by class

--- src/code_step.js
+++ src/code_step.js
@@ -1,46 +1,78 @@
-function codeStepInit(object) {
+function Cs() {
+  var self = this;
 
-  function setProperty(propName) {
-    console.debug('setProperty', propName);
+  self.initClass = initClass;
+  self.initClasses = initClasses;
 
-    object._cs[propName] = object[propName];
+  return self;
 
-    Object.defineProperty(object, propName, {
-      get: function() {
-        console.debug('get', propName, this._cs[propName]);
-        return this._cs[propName];
-      },
-      set: function(value) {
-        console.debug('set', propName, value, this._cs[propName]);
-        this._cs[propName] = value;
+  function initClass(className) {
+    var constructor = window[className];
+
+    window[className] = function() {
+      console.debug('constructor', className, arguments);
+
+      var object = Object.create(constructor.prototype);
+      constructor.apply(object, arguments);
+
+      initObject(object);
+      return object;
+    };
+  }
+
+  function initClasses(classeNames) {
+    for (var i = 0; i < classeNames.length; i++) {
+      initClass(initClass[i]);
+    }
+  }
+
+  function initObject(object) {
+
+    function setProperty(propName) {
+      console.debug('setProperty', propName);
+
+      object.cs[propName] = object[propName];
+
+      Object.defineProperty(object, propName, {
+        get: function() {
+          console.debug('get', propName, this.cs[propName]);
+          return this.cs[propName];
+        },
+        set: function(value) {
+          console.debug('set', propName, value, this.cs[propName]);
+          this.cs[propName] = value;
+        }
+      });
+    }
+
+    function setFunction(funcName) {
+      console.debug('setFunction', funcName);
+
+      object.cs[funcName] = object[funcName];
+
+      object[funcName] = function() {
+        console.debug('call', funcName, arguments);
+        object.cs[funcName].apply(object, arguments);
+      };
+    }
+
+    console.debug('codeStepInit');
+
+    object.cs = {};
+
+    for (var propName in object) {
+      if (propName === 'cs') {
+        continue;
       }
-    });
-  }
 
-  function setFunction(funcName) {
-    console.debug('setFunction', funcName);
-
-    object._cs[funcName] = object[funcName];
-
-    object[funcName] = function() {
-      console.debug('call', funcName, arguments);
-      object._cs[funcName].apply(object, arguments)
+      if (typeof(object[propName]) === 'function') {
+        setFunction(propName);
+      } else {
+        setProperty(propName);
+      }
     }
   }
 
-  console.debug('codeStepInit');
-
-  object._cs = {};
-
-  for (var propName in object) {
-    if (propName === '_cs') {
-      continue;
-    }
-
-    if (typeof(object[propName]) === 'function') {
-      setFunction(propName)
-    } else {
-      setProperty(propName);
-    }
-  }
 }
+
+window.cs = new Cs();
--- src/test_data.js
+++ src/test_data.js
@@ -1,24 +1,27 @@
-function TestClass(param1, param2) {
+window.TestClass = function(param1, param2) {
   //! main
   this.propertyString = 'value';                          //! string
   this.propertyNumber = 17;                               //! number
   this.propertyArray = ['value1', 'value2', 'value3'];    //! array
-  this.propertyObject = {key1: 'value1', key2: 'value2'}; //! object
+  this.propertyObject = {                                 //! object
+    key1: 'value1',
+    key2: 'value2'
+  };
 
   this.propertyDate = new Date();                         //! date
   this.propertyUndefined = undefined;                     //! undefined
   this.propertyNull = null;                               //! null
   this.propertyNan = NaN;                                 //! NaN
-}
+};
 
-TestClass.prototype.method1 = function() {
+window.TestClass.prototype.method1 = function() {
   console.log('call method1');
-}
+};
 
-TestClass.prototype.method2 = function(param1) {
+window.TestClass.prototype.method2 = function(param1) {
   console.log('call method2', param1);
-}
+};
 
-TestClass.prototype.method3 = function(param1, param2) {
+window.TestClass.prototype.method3 = function(param1, param2) {
   console.log('call method3', param1, param2);
-}
+};
